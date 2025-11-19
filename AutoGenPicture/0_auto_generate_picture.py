ee2f8c0fb820a6305b15289631d15e8bef59a85b
coast <coast@gaoding.com>
2025-11-19 12:07:39 +0800

    m

--- AutoGenPicture/0_auto_generate_picture.py
+++ AutoGenPicture/0_auto_generate_picture.py
@@ -8,11 +8,12 @@ import requests
 app_id = 'dh2511ujbi5lzy3z84'
 app_secret = 'e22557f526d04a387e104352f723c99d'
 version = '2.0.0'
-cur_timestamp = int(time.time())
+MAX_REQUEST_FOR_STATUS_CNT = 30
+MAX_REQUEST_FOR_GENERATION_CNT = 10
 
-input_img_paths = [#'https://m.media-amazon.com/images/I/71CZ9WJJviL._AC_SL1500_.jpg',
-                   # 'https://m.media-amazon.com/images/I/71M6+8Xn2YL._AC_SL1500_.jpg']
-                    'https://m.media-amazon.com/images/I/81KZJl+1xtL._AC_SL1500_.jpg']
+gen_img_url = 'https://dhd.douhuiai.com/api/aiart/doGennanoimg'
+get_img_url = 'https://dhd.douhuiai.com/api/ai/getimgstatus'
+
 
 def download_image_by_url(image_url, save_path="downloaded_image.jpg"):
     """
@@ -65,77 +66,88 @@ def download_image_by_url(image_url, save_path="downloaded_image.jpg"):
             print(f"错误状态码：{response.status_code}")
         return False
 
-# 认证信息
-authorization = {
-    'version': version,
-    'timestamp': cur_timestamp,
-    'appid': app_id,
-    'sign': hashlib.md5(
-        f"{app_id}{cur_timestamp}{version}{app_secret}".encode("utf-8")
-    ).hexdigest()
-}
 
-# 请求数据
-gen_img_url = 'https://dhd.douhuiai.com/api/aiart/doGennanoimg'
-get_img_url = 'https://dhd.douhuiai.com/api/ai/getimgstatus'
+def generate_img(input_img_path='https://m.media-amazon.com/images/I/81KZJl+1xtL._AC_SL1500_.jpg'):
+    input_img_paths = [input_img_path]
+    data = {
+        'dhAiType': 'nanoimg', #'txt2img',
+        'dhPrompt': '给输入的图片加个或者换一个合适的背景，背景虚化一下，多加一些点缀，透明度40%；如果图片中有人物，就给人物换张脸；其他部分不要变动',
+        'dhInputImgs': json.dumps(input_img_paths)
+    }
 
-data = {
-    'dhAiType': 'nanoimg', #'txt2img',
-    'dhPrompt': '给输入的图片加个或者换一个合适的背景，背景虚化一下，多加一些点缀，透明度40%；如果图片中有人物，就给人物换张脸；其他部分不要变动',
-    'dhInputImgs': json.dumps(input_img_paths)
-}
+    image_is_ready = False
+    request_for_generation_cnt = 1
+    while (not image_is_ready) and request_for_generation_cnt <= MAX_REQUEST_FOR_GENERATION_CNT:
+        print(f'第{request_for_generation_cnt}次请求生成图片')
 
-headers = {
-    'authorization': json.dumps(authorization),
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-}
+        # 认证信息
+        cur_timestamp = int(time.time())
+        authorization = {
+            'version': version,
+            'timestamp': cur_timestamp,
+            'appid': app_id,
+            'sign': hashlib.md5(
+                f"{app_id}{cur_timestamp}{version}{app_secret}".encode("utf-8")
+            ).hexdigest()
+        }
 
-try:
-    gen_img_response = requests.post(
-        url=gen_img_url,
-        data=data,
-        headers=headers,
-        timeout=60
-    )
-    gen_img_response_json = json.loads(gen_img_response.text)
-    print(gen_img_response_json)
+        headers = {
+            'authorization': json.dumps(authorization),
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        }
+        try:
+            gen_img_response = requests.post(
+                url=gen_img_url,
+                data=data,
+                headers=headers,
+                timeout=60
+            )
+            gen_img_response_json = json.loads(gen_img_response.text)
+            print(gen_img_response_json)
 
-    
-    if gen_img_response_json['status'] == 200:
-        img_paths = gen_img_response_json['imgs']['imgs']
-        print('下载并保存原图片')
-        for img_idx, input_img_path in enumerate(input_img_paths):
-            download_image_by_url(input_img_path, f'images/input/{img_idx}.jpg')
+            if gen_img_response_json['status'] == 200:
+                if request_for_generation_cnt == 1:
+                    print('下载并保存原图片')
+                    for img_idx, input_img_path in enumerate(input_img_paths):
+                        download_image_by_url(input_img_path, f'images/input/{img_idx}.jpg')
 
-        request_cnt = 1
-        uuid = gen_img_response_json['uuid']
-        image_is_generating = True
-        while image_is_generating:
-            print(f'轮询图片生成状态, 第{request_cnt}次...')
-            try:
-                get_img_response = requests.get(f'{get_img_url}?uuid={uuid}&source=api', headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"})
-                get_img_response_json = json.loads(get_img_response.text)
-                get_img_status = get_img_response_json['status']
+                request_for_status_cnt = 1
+                uuid = gen_img_response_json['uuid']
+                
+                while (not image_is_ready) and request_for_status_cnt <= MAX_REQUEST_FOR_STATUS_CNT:
+                    print(f'轮询图片生成状态, 第{request_for_status_cnt}次...')
+                    try:
+                        get_img_response = requests.get(f'{get_img_url}?uuid={uuid}&source=api', headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"})
+                        get_img_response_json = json.loads(get_img_response.text)
+                        get_img_status = get_img_response_json['status']
 
-                image_is_generating = False
-                if get_img_status == 200:
-                    print(f'出图成功！{get_img_response_json}')
-                    for img_idx, img_path in enumerate(get_img_response_json['imglist']):
-                        print(f'img_path:{img_path}')
-                        download_image_by_url(img_path, f'images/output/{img_idx}.{img_path.split(".")[-1]}')
-                elif get_img_status == -200:
-                    image_is_generating = True
-                elif get_img_status == 404:
-                    print("任务过期或不存在")
-                elif  get_img_status == 500:
-                    print("出图失败")
+                        if get_img_status == 200:
+                            print(f'出图成功！{get_img_response_json}')
+                            for img_idx, img_path in enumerate(get_img_response_json['imglist']):
+                                print(f'img_path:{img_path}')
+                                print('下载并保存ai图片')
+                                download_image_by_url(img_path, f'images/output/{img_idx}.{img_path.split(".")[-1]}')
+                                image_is_ready = True
+                        #elif get_img_status == -200:
+                        #    image_is_generating = True
+                        elif get_img_status == 404:
+                            print("任务过期或不存在")
+                            break
+                        elif  get_img_status == 500:
+                            print("出图失败")
+                            break
 
-                time.sleep(10)
-                request_cnt += 1
-            except requests.exceptions.RequestException as e:
-                print(f'轮询图片状态的请求失败:{e}')
-    
-except requests.exceptions.RequestException as e:
-    print(f'生成图片的请求失败:{e}')
+                        time.sleep(10)
+                        request_for_status_cnt += 1
+                    except requests.exceptions.RequestException as e:
+                        print(f'轮询图片状态的请求失败:{e}')
+            
+        except requests.exceptions.RequestException as e:
+            print(f'生成图片的请求失败:{e}')
+        
+        request_for_generation_cnt += 1
 
+
+if __name__ == '__main__':
+    generate_img(input_img_path='https://m.media-amazon.com/images/I/81KZJl+1xtL._AC_SL1500_.jpg')
